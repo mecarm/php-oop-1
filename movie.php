cc71cdf4681f46e553e40d4129dd35ba4c3f356a
mecarm <melecarmine99@gmail.com>
2022-12-16 14:43:00 +0100

    Uso costruttore per associare instanza

--- movie.php
+++ movie.php
@@ -5,6 +5,10 @@ class Movie{
     public $type;
     public $poster;
 
-    
+    public function __construct($_title, $_type, $_poster){
+        $this -> title = $_title;
+        $this -> type = $_type;
+        $this -> poster = $_poster;
+    }
 }
 ?>
