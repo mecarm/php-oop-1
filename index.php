d83b735ea76993f4efa9793bf7820889433cb080
mecarm <melecarmine99@gmail.com>
2022-12-16 14:32:24 +0100

    Creo classe con instanza

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//HOMEPAGE
+
+//Importo la classe da movie.php
+include_once __DIR__ . '/movie.php';
+
+$Movie1 = new Movie();
+
+$Movie1 -> title = 'Batman';
+$Movie1 -> type = 'Fantasy';
+$Movie1 -> poster = 'https://upload.wikimedia.org/wikipedia/it/c/c4/The_Batman_%28film%29.png';
+
+var_dump($Movie1);
+?>
---
+++ movie.php
@@ -0,0 +1,10 @@
+<?php
+
+class Movie{
+    public $title;
+    public $type;
+    public $poster;
+
+    
+}
+?>
